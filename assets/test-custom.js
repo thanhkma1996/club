f73b425a2790bf5767555b6ccb17d00c77733336
thanh nguyen <thanh.nguyen@digitaloutlook.com.au>
2021-06-25 17:38:13 +0700

    TUC-1 update all my address

--- assets/test-custom.js
+++ assets/test-custom.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     $('.account-recovery-toggle').click(function(){
         $(this).parents('body').addClass('forgot-password');
     });
+
+    // $('.js-address-delete').on('click', function() {
+    //     var $el = $(this);
+    //     var formId = $el.data('form-id');
+    //     var confirmMessage = $el.data('confirm-message');
+    //     if (confirm(confirmMessage || 'Are you sure you wish to delete this address?')) {
+    //       Shopify.postLink('/account/addresses/' + formId, {parameters: {_method: 'delete'}});
+    //       setTimeout(function () {
+    //         window.location.href = '/account/addresses';
+    //       }, 5000)
+    //     }
+    //   });
 });
